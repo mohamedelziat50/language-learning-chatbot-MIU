b3c83b1e139a7d2c59c5e7502bc4ed12656081f0
mohamedelziat50 <mohamedelziat50@gmail.com>
2025-10-11 23:51:45 +0300

    Add Topics page and update sidebar links for navigation

--- app/views/partials/sidebar.php
+++ app/views/partials/sidebar.php
@@ -10,8 +10,8 @@ $currentPage = basename($_SERVER['PHP_SELF']);
             <a href="dashboard.php"<?php if ($currentPage == 'dashboard.php') echo ' class="active"'; ?>><i class="fas fa-home"></i> Dashboard</a>
             <a href="docs.php"<?php if ($currentPage == 'docs.php') echo ' class="active"'; ?>><i class="fas fa-file-alt"></i> Documents</a>
             <a href="quiz.php"<?php if ($currentPage == 'quiz.php') echo ' class="active"'; ?>><i class="fas fa-question-circle"></i> Quiz</a>
+            <a href="topics.php"<?php if ($currentPage == 'topics.php') echo ' class="active"'; ?>><i class="fas fa-book"></i> Topics</a>
             <a href="#"<?php if ($currentPage == 'chatbot.php') echo ' class="active"'; ?>><i class="fas fa-robot"></i> Chatbot</a>
-            <a href="#"<?php if ($currentPage == 'topics.php') echo ' class="active"'; ?>><i class="fas fa-book"></i> Topics</a>
             <a href="#"<?php if ($currentPage == 'discussion.php') echo ' class="active"'; ?>><i class="fas fa-comments"></i> Discussion</a>
         </div>
         <div class="bottom-links">
