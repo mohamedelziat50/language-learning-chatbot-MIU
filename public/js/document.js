d3e1df5496245c3f84c9092701a0f02b68464f63
mohamedelziat50 <mohamedelziat50@gmail.com>
2025-12-22 21:46:56 +0200

    testing smth

--- public/js/document.js
+++ public/js/document.js
@@ -311,9 +311,9 @@ class DocumentEditor {
         try {
             let url;
             if (documentId) {
-                url = `../../app/index.php/documents/${documentId}/update`;
+                url = `/app/index.php/documents/${documentId}/update`;
             } else {
-                url = `../../app/index.php/documents/create`;
+                url = `/app/index.php/documents/create`;
             }
 
             const response = await fetch(url, {
